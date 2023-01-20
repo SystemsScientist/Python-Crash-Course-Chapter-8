948538bd6be53c3185cdce6a5a9ffc842101488f
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-19 19:37:28 -0600

    Adding greeter.py file

---
+++ Examples/greeter.py
@@ -0,0 +1,11 @@
+# greeter.py, Chapter 8, Python Crash Course
+# program displays a simple message
+
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+
+greet_user()
+
+
+

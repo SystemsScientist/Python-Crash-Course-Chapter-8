24ecb7f255a9c8b51655982f403dbae1500c8c8e
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-23 17:41:56 -0600

    Adding pets.py file

---
+++ Examples/pets.py
@@ -0,0 +1,13 @@
+# pets.py, Chapter 8, Python Crash Course
+# program calls a function with positional 
+# arguments
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".\n")
+
+describe_pet('hamster', 'harry')
+
+
+

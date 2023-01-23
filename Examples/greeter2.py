19bdd692f1fff74f0fbf598be8fa542e6f0c849a
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-23 05:30:26 -0600

    Adding greeter2.py file

---
+++ Examples/greeter2.py
@@ -0,0 +1,11 @@
+# greeter2.py, Chapter 8, Python Crash Course
+# program calls function and prints user's name
+
+def greet_user(username):
+    """display a simple greeting"""
+    print("Hello, " + username.title() + "!")
+
+greet_user('jesse')
+
+
+

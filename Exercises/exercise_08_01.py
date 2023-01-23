38882e995945fa87c3cb23c7b93f87972ba4195e
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-23 05:38:13 -0600

    Adding exercise_08_01.py file

---
+++ Exercises/exercise_08_01.py
@@ -0,0 +1,15 @@
+# exercise_08_01.py, Chapter 8, Python Crash Course
+#
+# Message: Write a function called display_message()
+# that prints one sentence telling everyone what you 
+# are learning about in this chapter. Call the function,
+# and make sure the message displays correctly.
+
+def display_message(topic):
+    """display message"""
+    print("I am learning about " + topic + " in this chapter.")
+
+display_message('functions')
+
+
+

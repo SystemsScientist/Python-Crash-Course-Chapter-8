86fc9eb8c0a1f0a57771afd96307656f15dfe84d
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-23 17:46:23 -0600

    Adding pets2.py file

---
+++ Examples/pets2.py
@@ -0,0 +1,13 @@
+# pets2.py, Chapter 8, Python Crash Course
+# program executes multiple function calls
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+
+

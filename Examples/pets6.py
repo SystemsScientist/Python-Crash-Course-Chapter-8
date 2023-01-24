c76cba06b90ff6a00b69d449f64c8784f1eb8f69
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-24 17:02:41 -0600

    Adding pets6.py file

---
+++ Examples/pets6.py
@@ -0,0 +1,12 @@
+# pets6.py, Chapter 8, Python Crash Course
+# program produces an error
+
+def describe_pet(animal_type, pet_name):
+    """display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet()
+
+
+

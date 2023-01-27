0ce813d4ecab6115f0ef138febb061558eed7d10
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-26 19:19:29 -0600

    Updating exercise_08_07.py

--- Exercises/exercise_08_07.py
+++ Exercises/exercise_08_07.py
@@ -26,5 +26,24 @@ print(artist_album)
 artist_album = make_album(artist_name = 'joe satriani', album_title = 'the extremist')
 print(artist_album)
 
+# Add an optional parameter to make_album() that
+# allows you to store the number of tracks on an
+# album. If the calling line includes a value for
+# the number of tracks, add that value to the album's
+# dictionary. Make at least one new function call 
+# that includes the number of tracks on an album.
+
+def make_album(artist_name, album_title, number_of_tracks = ''):
+    album = {'artist': artist_name.title(), 'album': album_title.title()}
+    if number_of_tracks:
+        album['number_of_tracks'] = number_of_tracks
+    return album
+
+print("\nList of Dictionaries of Artists and Albums: ")
+print("----------------------------------------------")
+
+artist_album = make_album(artist_name = 'michael jackson', album_title = 'thriller', number_of_tracks = 7)
+print(artist_album)
+
 
 
